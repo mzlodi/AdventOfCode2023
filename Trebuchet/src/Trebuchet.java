8581f707fa5f4335e0d9744591639cc82d30097d
Marija Zlodi <marija.zlodi01@gmail.com>
2023-12-01 20:15:38 +0100

    Day 1 - added different code paths for part one and part two

--- Trebuchet/src/Trebuchet.java
+++ Trebuchet/src/Trebuchet.java
@@ -7,15 +7,19 @@ import java.util.Map;
 
 public class Trebuchet {
     public static void main(String[] args) {
-        List<String> inputStrings = getInputString();
-        List<Integer> numsFromStringList = extractFirstAndLast(inputStrings);
+        Boolean isTest = false;
+        Boolean firstPartOfChallenge = false;
+
+        List<String> inputStrings = getInputString(isTest);
+        List<Integer> numsFromStringList = extractFirstAndLast(inputStrings, firstPartOfChallenge);
+
         Integer sumOfAllIntegers = sumOfInts(numsFromStringList);
 
         System.out.println("The sum is " + sumOfAllIntegers);
     }
 
-    private static List<String> getInputString() {
-        String filePath = "input.txt";
+    private static List<String> getInputString(Boolean isTest) {
+        String filePath = isTest == true ? "testInput.txt" : "input.txt";
 
         try {
             FileReader fileReader = new FileReader(filePath);
@@ -38,9 +42,26 @@ public class Trebuchet {
         }
     }
 
-    private static List<Integer> extractFirstAndLast(List<String> inputStrings) {
+    private static List<Integer> extractFirstAndLast(List<String> inputStrings, Boolean firstPartOfChallenge) {
         List<Integer> numsFromStringList = new ArrayList<>();
-        List<String> replacedWordsList = new ArrayList<>();
+        List<String> stringForExtractingNumbers = firstPartOfChallenge ? inputStrings : replaceWordsWithNumbers(inputStrings);
+
+        for (String s : stringForExtractingNumbers) {
+            String strNums = s.replaceAll("[a-zA-Z]", "");
+            String firstAndLastNumString;
+            char firstNum = strNums.charAt(0);
+            char lastNum = strNums.charAt(strNums.length() - 1);
+            firstAndLastNumString = String.valueOf(firstNum) + String.valueOf(lastNum);
+
+            numsFromStringList.add(Integer.parseInt(firstAndLastNumString));
+        }
+
+        return numsFromStringList;
+    }
+
+    private static List<String> replaceWordsWithNumbers(List<String> inputStrings){
+        List<String> stringForExtractingNumbers = new ArrayList<>();
+
         Map<String, String> wordToNumberMap = Map.ofEntries(
                 Map.entry("one", "o1e"),
                 Map.entry("two", "t2o"),
@@ -57,20 +78,10 @@ public class Trebuchet {
             for (Map.Entry<String, String> entry : wordToNumberMap.entrySet()) {
                 s = s.replaceAll(entry.getKey(), entry.getValue());
             }
-            replacedWordsList.add(s);
+            stringForExtractingNumbers.add(s);
         }
 
-        for (String s : replacedWordsList) {
-            String strNums = s.replaceAll("[a-zA-Z]", "");
-            String firstAndLastNumString;
-            char firstNum = strNums.charAt(0);
-            char lastNum = strNums.charAt(strNums.length() - 1);
-            firstAndLastNumString = String.valueOf(firstNum) + String.valueOf(lastNum);
-
-            numsFromStringList.add(Integer.parseInt(firstAndLastNumString));
-        }
-
-        return numsFromStringList;
+        return stringForExtractingNumbers;
     }
 
     private static Integer sumOfInts(List<Integer> numsFromStringList) {
